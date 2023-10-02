33cc601dcbcbae129f44980941f7401741946d41
Denys Tryfonov <trifonowden123@gmail.com>
2023-10-02 18:43:14 +0300

    r

--- src/homework/1.tsx
+++ src/homework/1.tsx
@@ -1,6 +1,10 @@
 import React, { useEffect, useRef } from 'react';
 
 // Опишіть Props
+
+type Props = {
+  
+}
 export function Observer({ children, onContentEndVisible }: Props) {
   // Вкажіть правильний тип для useRef зверніть увагу, в який DOM елемент ми його передаємо
   const endContentRef = useRef(null);
